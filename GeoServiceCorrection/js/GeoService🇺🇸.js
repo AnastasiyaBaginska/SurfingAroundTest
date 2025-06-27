e4b07671db0d1eb1e106d040fb7f8bf8590d6ec4
AnastasiyaBaginska <111028702+AnastasiyaBaginska@users.noreply.github.com>
2025-06-28 01:14:42 +0800

    Create GeoService🇺🇸.js
    The script-response-body file of GeoService🇺🇸.qxrewrite

---
+++ GeoServiceCorrection/js/GeoService🇺🇸.js
@@ -0,0 +1,17 @@
+// The script-response-body file of GeoService🇺🇸.qxrewrite
+let GeoCountryCode = "US";
+
+if (typeof $argument != "undefined") {
+    let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
+    console.log(JSON.stringify(arg));
+    GeoCountryCode = arg.GeoCountryCode;
+};
+
+const url = $request.url;
+var body = $response.body;
+
+const path1 = "/pep/gcc";
+
+if (url.indexOf(path1) != -1) body = GeoCountryCode;
+
+$done({body});
